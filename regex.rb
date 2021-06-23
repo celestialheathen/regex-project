fcf7ac1a3d3220266fc7f8de27d0cdccc63a0248
celestialheathen <jingchaoyu@hotmail.com>
2021-06-23 11:56:16 -0400

    addresses

--- regex.rb
+++ regex.rb
@@ -3,15 +3,7 @@ info = File.new('info.txt')
 # Names
 # puts info.read.scan(/^[a-z]+\.?\s[a-z]+.?\s[a-z]+$/i)
 
+#Addresses
+# puts info.read.scan(/^\d+-?\d*\s[a-z]+\s\w+.*\n[a-z]+.*/i)
 
-# puts info.read.scan(/^\(?\d+.+\d+.\d+-?[a-z]*-?[a-z]*/i)
-
-# 718–704–5199
-# (347) 206–0091
-# 9293365110
-# 1–800-Call-Me
-
-puts info.read.scan(/^[\(\d]?-?\d{3}.*[\da-z]+-?[\da-z]+$/i)
-
-# puts info.read.scan(/\d-\d{3}-[a-z]+-[a-z]+/i)
 info.close  
