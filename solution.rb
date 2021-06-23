ed05d1b5b32ecbb41c1f5719d69edc2e66dd94d9
celestialheathen <jingchaoyu@hotmail.com>
2021-06-23 15:53:23 -0400

    added solution

---
+++ solution.rb
@@ -0,0 +1,8 @@
+# Names
+# puts info.read.scan(/^[a-z]+\.?\s[a-z]+.?\s[a-z]+$/i)
+
+# Addresses
+# puts info.read.scan(/^\d+-?\d*\s[a-z]+\s\w+.*\n[a-z]+.*/i)
+
+# Phone numbers
+# puts info.read.scan(/^\d?[-\(]?\d{3}[-\)\s]*\w+-?\w+$/)
