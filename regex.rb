6d1e828c585842821cc928e72bd9590d24c1b247
celestialheathen <jingchaoyu@hotmail.com>
2021-06-23 12:17:50 -0400

    phone numbers

--- regex.rb
+++ regex.rb
@@ -3,7 +3,10 @@ info = File.new('info.txt')
 # Names
 # puts info.read.scan(/^[a-z]+\.?\s[a-z]+.?\s[a-z]+$/i)
 
-#Addresses
+# Addresses
 # puts info.read.scan(/^\d+-?\d*\s[a-z]+\s\w+.*\n[a-z]+.*/i)
+
+# Phone numbers
+puts info.read.scan(/^\d?[-\(]?\d{3}[-\)\s]*\w+-?\w+$/)
 
 info.close  
